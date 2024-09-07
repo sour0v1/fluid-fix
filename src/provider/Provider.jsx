a9d0b6b32d5dc1c36c7aaaf04bed1e36fc491bd4
Sourov <sourovdas730@gmail.com>
2024-09-07 11:40:16 +0600

    add : user creation and sign in function

--- src/provider/Provider.jsx
+++ src/provider/Provider.jsx
@@ -1,13 +1,34 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import React, { createContext, useState } from 'react';
+import auth from '../firebase.config';
 
 // create context
 export const AppContext = createContext(null);
 
 const Provider = ({children}) => {
     const [user, setUser] = useState(null);
+    const googleProvider = new GoogleAuthProvider;
+
+    // create user by email and password
+    const createUserByEmailPass = (email, pass) => {
+        return createUserWithEmailAndPassword(auth, email, pass);
+    }
+
+    // create or login with Google
+    const createUserByGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // sign in with email and password
+    const signInWithEmailPass = (email, pass) => {
+        return signInWithEmailAndPassword(auth, email, pass);
+    }
 
     const value = {
         user,
+        createUserByEmailPass,
+        createUserByGoogle,
+        signInWithEmailPass,
     }
     return (
         <AppContext.Provider value={value}>
