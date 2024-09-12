53d518c8ff57a6e269fbd63bed0b88b6dba66934
Sourov <sourovdas730@gmail.com>
2024-09-12 08:22:34 +0600

    add : description and location field

--- src/dashboard/seller/AddService.jsx
+++ src/dashboard/seller/AddService.jsx
@@ -5,6 +5,7 @@ import { RiArrowDropDownLine } from 'react-icons/ri';
 const AddService = () => {
     const [open, setOpen] = useState(false);
     const [category, setCategory] = useState('Category');
+    const [isFocused, setIsFocused] = useState(false);
     return (
         <div className='z-20 bg-white'>
             <div className='flex justify-between items-center border-b pb-4'>
@@ -31,6 +32,25 @@ const AddService = () => {
                                 <button onClick={() => {setCategory('Category 4'); setOpen(!open)}} type='button'  className='hover:bg-[#094074] hover:bg-opacity-10 w-full py-2 flex flex-col justify-start items-center'>Catgory 4</button>
                                 <button onClick={() => {setCategory('Category 5'); setOpen(!open)}} type='button'  className='hover:bg-[#094074] hover:bg-opacity-10 w-full py-2 flex flex-col justify-start items-center'>Catgory 5</button>
                             </div>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col gap-1 lg:col-span-2'>
+                        <span>Description</span>
+                        <textarea className='outline-none resize-none h-24 px-3 py-2 rounded-lg focus:border-2 focus:border-[#094074] bg-[#094074] bg-opacity-10'>
+                        </textarea>
+                    </div>
+
+                    <div className='flex flex-col gap-1'>
+                        <span>Price</span>
+                        <input className='py-2 rounded-full px-6 bg-[#094074] bg-opacity-10 outline-none focus:border-2 focus:border-[#094074]' type="text" placeholder='Taka' />
+                    </div>
+
+                    <div className='flex flex-col gap-1'>
+                        <span>Service Area</span>
+                        <input onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className='py-2 rounded-full px-6 bg-[#094074] bg-opacity-10 outline-none focus:border-2 focus:border-[#094074]' type="text" placeholder='Type area' />
+                        <div className={`bg-white shadow-lg w-full rounded-b-lg h-24 ${isFocused ? 'block' : 'hidden'}`}>
+
                         </div>
                     </div>
                 </form>
